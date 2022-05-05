77b88558de36a3a4fc4d92746b283f90450d7d88
Bekzod <bekzodturgunoff@gmail.com>
2022-05-05 22:19:33 +0500

    second commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// const toggle = document.querySelector("#toggle");
+
+// toggle.addEventListener("click", modeSwitch);
+
+// let isLight = true;
+
+// console.log(isLight);
+
+// function modeSwitch() {
+//   isLight = !isLight;
+//   let root = document.body;
+
+//   isLight ? (toggle.innerText = "🌞") : (toggle.innerText = "🌚");
+
+//   root.classList.toggle("lightMode");
+// }
